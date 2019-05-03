6dc36bf343b540c7a6b399be97bd183ae5fd5982
IbraCadaBra010101 <ibma010101@gmail.com>
2019-05-03 13:39:42 +0200

    extended mock with service class

--- src/app/shared/services/mock-transaction.service.ts
+++ src/app/shared/services/mock-transaction.service.ts
@@ -1,14 +1,16 @@
 import {Injectable} from '@angular/core';
 import {Account} from '../interfaces/account';
+import {AccountTransactionsService} from './account-transactions.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockTransactionService {
+export class MockTransactionService extends AccountTransactionsService {
   customer: Account = {customerName: 'Oskar Olsson', balance: 5000};
   customer2: Account = {customerName: 'Skatteverkets Bank Giro', balance: 0};
 
   constructor() {
+    super();
   }
 
   deposit() {
@@ -16,6 +18,7 @@ export class MockTransactionService {
 
 
   getBalance() {
+    return 0;
   }
 
 
